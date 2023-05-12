4bc0f506a842eac6e8f1da436d8359faa3069943
veramazzetta <vmazzetta1479@student.pps.net>
2023-05-12 11:04:49 -0700

    working

--- Organize for phone/navigation.swift
+++ Organize for phone/navigation.swift
@@ -6,11 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct Landmark: Hashable, Codable {
+struct typeList: Hashable, Codable {
     var id: Int
     var name: String
-    var park: String
-    var state: String
+    var type: String
     var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
 }
+
